abf5c681f9076fb34d26c78f7455d79dea7c1b20
Administrator <124682191+judah2023@users.noreply.github.com>
2023-07-31 21:48:16 +0900

    (2023.07.31) Hash

--- DS&Algorithms/Week3/01/Quick_Sort (2023.07.27).h
+++ DS&Algorithms/Week3/01/Quick_Sort (2023.07.27).h
@@ -1,1 +1,91 @@
 ﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#pragma region Counting_Sort
+
+// 데이터의 값을 직접 비교하는 것이 아닌 값의 개수를 저장하여 정렬하는 알고리즘
+enum Constants
+{
+	SIZE = 10000
+};
+
+vector<int> vec(SIZE, 0);
+
+#pragma endregion
+
+#pragma region Quick_Sort
+
+// 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고
+// 한 쪽에는 기준잠보다 작은 항목들이 위치하고 다른쪽에는 기준점보다 큰 항목들이 위치한다.
+
+// 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여,
+// 모든 배열이 기본 배열이 될 때까지 반복하는 알고리즘
+
+void MideanToHigh(vector<int>& vec, int low, int high)
+{
+	int mid = (low + high) / 2;
+	if (vec[low] < vec[mid])
+	{
+		if (vec[mid] < vec[high])
+			swap(vec[mid], vec[high]);
+		else if (vec[high] < vec[low])
+			swap(vec[low], vec[high]);
+	}
+	else
+	{
+		if (vec[mid] > vec[high])
+			swap(vec[mid], vec[high]);
+		else if (vec[high] > vec[low])
+			swap(vec[low], vec[high]);
+	}
+}
+
+int Partition(vector<int>& vec, int low, int high)
+{
+	if (high - low > 1)
+		MideanToHigh(vec, low, high);
+
+	int pivot = vec[high];
+	int i = low - 1;
+	for (int j = low; j < high; j++)
+		if (vec[j] < pivot)
+			swap(vec[++i], vec[j]);
+	swap(vec[++i], vec[high]);
+	return i;
+}
+
+void QuickSort(vector<int>& vec, int low, int high)
+{
+	if (low < high)
+	{
+		int pivot = Partition(vec, low, high);
+
+		QuickSort(vec, low, pivot - 1);
+		QuickSort(vec, pivot + 1, high);
+	}
+}
+
+#pragma endregion
+
+void PrintVector(vector<int>& vec)
+{
+	cout << "[ ";
+	for (auto& it : vec)
+		cout << it << " ";
+	cout << "]" << endl;
+}
+
+int main()
+{
+	vector<int> vec = { 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+	PrintVector(vec);
+	QuickSort(vec, 0, vec.size() - 1);
+	PrintVector(vec);
+
+	return 0;
+}
--- DS&Algorithms/Week3/01/Source.cpp
+++ DS&Algorithms/Week3/01/Source.cpp
@@ -1,74 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
+#include <list>
 #include <cmath>
 
 using namespace std;
-
-#pragma region Counting_Sort
-
-// 데이터의 값을 직접 비교하는 것이 아닌 값의 개수를 저장하여 정렬하는 알고리즘
-enum Constants
-{
-	SIZE = 10000
-};
-
-vector<int> vec(SIZE, 0);
-
-#pragma endregion
-
-#pragma region Quick_Sort
-
-// 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고
-// 한 쪽에는 기준잠보다 작은 항목들이 위치하고 다른쪽에는 기준점보다 큰 항목들이 위치한다.
-
-// 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여,
-// 모든 배열이 기본 배열이 될 때까지 반복하는 알고리즘
-
-void MideanToHigh(vector<int>& vec, int low, int high)
-{
-	int mid = (low + high) / 2;
-	if (vec[low] < vec[mid])
-	{
-		if (vec[mid] < vec[high])
-			swap(vec[mid], vec[high]);
-		else if (vec[high] < vec[low])
-			swap(vec[low], vec[high]);
-	}
-	else
-	{
-		if (vec[mid] > vec[high])
-			swap(vec[mid], vec[high]);
-		else if (vec[high] > vec[low])
-			swap(vec[low], vec[high]);
-	}
-}
-
-int Partition(vector<int>& vec, int low, int high)
-{
-	if (high - low > 1)
-		MideanToHigh(vec, low, high);
-
-	int pivot = vec[high];
-	int i = low - 1;
-	for (int j = low; j < high; j++)
-		if (vec[j] < pivot)
-			swap(vec[++i], vec[j]);
-	swap(vec[++i], vec[high]);
-	return i;
-}
-
-void QuickSort(vector<int>& vec, int low, int high)
-{
-	if (low < high)
-	{
-		int pivot = Partition(vec, low, high);
-
-		QuickSort(vec, low, pivot - 1);
-		QuickSort(vec, pivot + 1, high);
-	}
-}
-
-#pragma endregion
 
 #pragma region Hash_Table
 
@@ -118,7 +53,9 @@ class HashTable
 	};
 
 private:
-	Bucket bucket[SIZE];
+	vector<Bucket> bucket;
+	list<int> keyList;
+	size_t size;
 
 	Node* CreateNode(int key, int value)
 	{
@@ -130,65 +67,157 @@ private:
 		return newNode;
 	}
 
-	void InsertNode(Bucket& curr, Node* newNode)
+	void InsertNode(size_t idx, Node* newNode)
 	{
-		newNode->next = curr.head;
-		curr.head = newNode;
+		Bucket& currBucket = bucket[idx];
+		if (currBucket.count != 0)
+		{
+			newNode->next = currBucket.head;
+		}
+		currBucket.head = newNode;
+		currBucket.count++;
 	}
 
 public:
 
 	HashTable()
 	{
-		for (int i = 0; i < 5; i++)
+		bucket.resize(SIZE);
+		for (int i = 0; i < SIZE; i++)
 		{
 			bucket[i].count = 0;
 			bucket[i].head = nullptr;
 		}
+		size = 0;
+	}
+
+	~HashTable()
+	{
+		Clear();
 	}
 
 	size_t HashFunction(int key)
 	{
-		size_t hash = SIZE * fmod((key * 0.6180339887), 1);
-		return hash;
+		return (size_t)(SIZE * fmod((key * 0.6180339887), 1));
 	}
 
 	void Insert(int key, int value)
 	{
 		size_t idx = HashFunction(key);
 		Node* newNode = CreateNode(key, value);
-		Bucket& currBucket = bucket[idx];
-		if (currBucket.count == 0)
+		InsertNode(idx, newNode);
+		keyList.push_back(key);
+		size++;
+	}
+
+	Node* Find(int key)
+	{
+		size_t idx = HashFunction(key);
+		Node* curr = bucket[idx].head;
+		while (curr != nullptr)
 		{
-			currBucket.head = newNode;
+			if (curr->key == key)
+				break;
+			curr = curr->next;
+		}
+		return curr;
+	}
+
+	void Search(int key)
+	{
+		Node* node = Find(key);
+		if (node == nullptr)
+		{
+			cout << "This key(" << key << ") is Invaild!" << endl;
 		}
 		else
 		{
-			InsertNode(currBucket, newNode);
+			cout << "[ Key, Value ] : " << "[ " << node->key << ", " << node->value << " ]" << endl;
 		}
-		currBucket.count++;
+	}
+
+	void Remove(int key)
+	{
+		Node* curr = Find(key);
+		if (curr == nullptr)
+			return;
+
+		size_t idx = HashFunction(key);
+		if (bucket[idx].count == 1)
+		{
+			bucket[idx].head = nullptr;
+		}
+		else
+		{
+			Node* prev = bucket[idx].head;
+			while (prev->next == curr)
+			{
+				prev = prev->next;
+			}
+
+			prev->next = curr->next;
+		}
+
+		size--;
+		bucket[idx].count--;
+		keyList.remove(curr->key);
+		delete curr;
+	}
+
+	int& operator[](int key)
+	{
+		Node* keyNode = Find(key);
+		if (keyNode == nullptr)
+		{
+			Insert(key, 0);
+			keyNode = Find(key);
+		}
+		return keyNode->value;
+	}
+
+	void Clear()
+	{
+		int key;
+		while (!keyList.empty())
+		{
+			key = keyList.front();
+			Remove(key);
+		}
 	}
 
 };
 
 #pragma endregion
 
-
-void PrintVector(vector<int>& vec)
-{
-	cout << "[ ";
-	for (auto& it : vec)
-		cout << it << " ";
-	cout << "]" << endl;
-}
-
 int main()
 {
-	vector<int> vec = { 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	HashTable hashMap;
+	hashMap.Insert(1, 11);
+	hashMap.Insert(2, 12);
+	hashMap.Insert(3, 13);
+	hashMap.Insert(4, 14);
+	hashMap.Insert(5, 15);
+	hashMap.Insert(6, 16);
 
-	PrintVector(vec);
-	QuickSort(vec, 0, vec.size() - 1);
-	PrintVector(vec);
+	hashMap[100] = 100;
+
+	hashMap.Search(1);
+	hashMap.Search(2);
+	hashMap.Search(3);
+	hashMap.Search(4);
+	hashMap.Search(5);
+	hashMap.Search(6);
+	hashMap.Search(100);
+
+	hashMap.Clear();
+
+	hashMap.Search(1);
+	hashMap.Search(2);
+	hashMap.Search(3);
+	hashMap.Search(4);
+	hashMap.Search(5);
+	hashMap.Search(6);
+	hashMap.Search(100);
 
 	return 0;
 }
